7dba35cef53df42f36cde2cda16eddcedc6636a5
Notstranger <vikashkasaudhan321@gmail.com>
2026-08-13 19:20:44 +0530

    Implement Circular Linked List with insert and display

---
+++ CircularLinkedList.java
@@ -0,0 +1,43 @@
+package Basics;
+
+public class CircularLinkedlist {
+
+    private Node head;
+    private Node tail;
+
+    public CircularLinkedlist(){
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insert(int val){
+        Node node = new Node(val);
+        if(head==null){
+            head = node;
+            tail = node;
+            return;
+        }
+        tail.next = node;
+        node.next = head;
+        tail = node;
+    }
+    public void display(){
+        Node node = head;
+        if(head!=null){
+            do{
+                System.out.print(node.val + " -> ");
+                node = node.next;
+            }while(node!=head);
+        }
+        System.out.println("Head");
+    }
+
+    private class Node{
+        int val;
+        Node next;
+
+        public Node(int val){
+            this.val = val;
+        }
+    }
+}
